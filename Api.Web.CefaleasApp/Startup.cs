dbbd66c1b313a87c0dd711a375e0f9ad5a153d04
Pablo Arjona Ruiz <pabloarjona98@gmail.com>
2024-01-12 14:52:39 +0100

    Other yml file

--- Api.Web.CefaleasApp/Startup.cs
+++ Api.Web.CefaleasApp/Startup.cs
@@ -25,6 +25,7 @@ namespace Api.Web.CefaleasApp
             services.AddControllers();
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
