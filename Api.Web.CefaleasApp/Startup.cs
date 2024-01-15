dfc91f9fcc3bdcfec6eeedb9b2fbfc20e8adbeb1
Pablo Arjona Ruiz <pabloarjona98@gmail.com>
2024-01-15 20:17:35 +0100

    Generando variable de entorno

--- Api.Web.CefaleasApp/Startup.cs
+++ Api.Web.CefaleasApp/Startup.cs
@@ -20,6 +20,7 @@ namespace Api.Web.CefaleasApp
         // This method gets called by the runtime. Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
+            var myHierarchicalConfig = Configuration["My:Hierarchical:Config:Data"];
             services.AddDataAccess(Configuration.GetConnectionString("DataBaseConnection"));
             //services.AddDataAccess(Configuration.GetConnectionString("LocalDatabaseConnection"));
             services.AddControllers();
